d587beddd662e70f25049dfa83f0671161da9cee
NatanFreitas <natanael_freitas@outlook.com>
2024-01-24 10:27:34 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,18 +31,20 @@ public class ChessMatch {
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
+    private void validateSourcePosition(Position position) {
+        if (!board.thereIsAPiece(position)){
+            throw new ChessException("There is no piece no source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible moves for the chosen piece");
+        }
 
+    }
     private Piece makeMove(Position source, Position target) {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
-    }
-
-    private void validateSourcePosition(Position source) {
-        if (!board.thereIsAPiece(source))
-            throw new ChessException("There is no piece no source position");
-
     }
 
 
